e23c5100b33aecd47bdf192fff60cd2b236fd4ad
Jun Lee <jh.lee6@alticast.com>
2017-06-19 17:24:15 +0900

    Connecting Widgets to Fragment

---
+++ CriminalIntent/app/src/main/java/com/bignerdbranch/andorid/criminalintent/CrimeFragment.java
@@ -0,0 +1,59 @@
+package com.bignerdbranch.andorid.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * Created by Jun on 2017-06-19.
+ */
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    /*
+    * Usually onCreate is 'protected method'
+    * However in here it is public method.
+    * Because theses methods are called by SOME ACTIVITY that hosts this fragment.
+    *
+    * Also, Fragment.onCreate does not inflate the view.
+    */
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // not used in this project
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setmTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // not used in here
+            }
+        });
+        return v;
+    }
+}

07dfc569c259d355154f0b114e46257a7a3b2e23
Jun Lee <jh.lee6@alticast.com>
2017-06-19 14:56:57 +0900

    Button Animation onClick

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,11 +1,15 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,14 +17,15 @@ import java.sql.BatchUpdateException;
 
 public class CheatActivity extends AppCompatActivity {
 
+    private static final String TAG = "CheatActivity";
     private static final String EXTRA_ANSWER_IS_TRUE =
             "com.bignerdranch.android.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN =
             "com.bignerdranch.android.geoquiz.answer_shown";
     private static final String CHEATER = "cheater";
-    private static final String TAG = "CheatActivity";
+    private static final String CLICKED = "clicked";
 
-
+    private boolean mIsClicked;
     private boolean mAnswerIsTrue;
 
     private TextView mAnswerTextView;
@@ -46,6 +51,36 @@ public class CheatActivity extends AppCompatActivity {
         setAnswerShownResult(true);
     }
 
+    private void buttonAnimationOnClick()
+    {
+        mShowAnswer = (Button)findViewById(R.id.show_answer_button);
+        mShowAnswer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mIsClicked = true;
+                setAnswerTextView(mAnswerTextView, mAnswerIsTrue);
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,18 +89,16 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerTextView = (TextView)findViewById(R.id.answer_text_view);
 
-        if(savedInstanceState != null){
-            mAnswerIsTrue = savedInstanceState.getBoolean(CHEATER, false);
-
-            setAnswerTextView(mAnswerTextView, mAnswerIsTrue);
+        if(savedInstanceState != null) {
+            mIsClicked = savedInstanceState.getBoolean(CLICKED, false);
+            if (mIsClicked) {
+                mShowAnswer = (Button) findViewById(R.id.show_answer_button);
+                mShowAnswer.setVisibility(View.INVISIBLE);
+                setAnswerTextView(mAnswerTextView, mAnswerIsTrue);
+            }
+            buttonAnimationOnClick();
         } else {
-            mShowAnswer = (Button)findViewById(R.id.show_answer_button);
-            mShowAnswer.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-                    setAnswerTextView(mAnswerTextView, mAnswerIsTrue);
-                }
-            });
+            buttonAnimationOnClick();
         }
     }
 
@@ -73,7 +106,7 @@ public class CheatActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle savedInstanceState){
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSavedInstanceState");
-        savedInstanceState.putBoolean(CHEATER, mAnswerIsTrue);
+        savedInstanceState.putBoolean(CLICKED, mIsClicked);
     }
 
     private void setAnswerShownResult(boolean isAnswerShown){

b293287580fcab8c7ecd128ace13863bf2fb37f4
Jun Lee <jh.lee6@alticast.com>
2017-06-20 17:07:50 +0900

    OnClickListener implemented on ViewHolder

--- CriminalIntent/app/src/main/java/com/bignerdbranch/andorid/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdbranch/andorid/criminalintent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -44,7 +45,8 @@ public class CrimeListFragment extends Fragment{
     }
 
     // ViewHolder
-    private class CrimeHolder extends RecyclerView.ViewHolder{
+    private class CrimeHolder extends RecyclerView.ViewHolder
+                implements View.OnClickListener{
         // public TextView mTitleTextView;
         private TextView mTitleTextView;
         private TextView mDateTextView;
@@ -54,6 +56,7 @@ public class CrimeListFragment extends Fragment{
 
         public CrimeHolder(View itemView){
             super(itemView);
+            itemView.setOnClickListener(this);
 
             //mTitleTextView = (TextView)itemView;
             mTitleTextView = (TextView)itemView.findViewById(R.id.list_item_crime_title_text_view);
@@ -66,6 +69,12 @@ public class CrimeListFragment extends Fragment{
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDateFormat());
             mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(), mCrime.getTitle() + " selected!", Toast.LENGTH_SHORT)
+                    .show();
         }
     }
 

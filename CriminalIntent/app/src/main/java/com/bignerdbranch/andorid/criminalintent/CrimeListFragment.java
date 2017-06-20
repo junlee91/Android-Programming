3752f4e21728a03aacd3f878b88a49351f0ba301
Jun Lee <jh.lee6@alticast.com>
2017-06-20 16:59:42 +0900

    RelativeLayout adjusted on list_item_crime

--- CriminalIntent/app/src/main/java/com/bignerdbranch/andorid/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdbranch/andorid/criminalintent/CrimeListFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.List;
@@ -44,12 +45,27 @@ public class CrimeListFragment extends Fragment{
 
     // ViewHolder
     private class CrimeHolder extends RecyclerView.ViewHolder{
-        public TextView mTitleTextView;
+        // public TextView mTitleTextView;
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private CheckBox mSolvedCheckBox;
+
+        private Crime mCrime;
 
         public CrimeHolder(View itemView){
             super(itemView);
 
-            mTitleTextView = (TextView)itemView;
+            //mTitleTextView = (TextView)itemView;
+            mTitleTextView = (TextView)itemView.findViewById(R.id.list_item_crime_title_text_view);
+            mDateTextView = (TextView)itemView.findViewById(R.id.list_item_crime_date_text_view);
+            mSolvedCheckBox = (CheckBox)itemView.findViewById(R.id.list_item_crime_solved_check_box);
+        }
+
+        public void bindCrime(Crime crime){
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDateFormat());
+            mSolvedCheckBox.setChecked(mCrime.isSolved());
         }
     }
 
@@ -65,7 +81,7 @@ public class CrimeListFragment extends Fragment{
         public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType){
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             View view = layoutInflater
-                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+                    .inflate(R.layout.list_item_crime, parent, false);
 
             return new CrimeHolder(view);
         }
@@ -73,7 +89,8 @@ public class CrimeListFragment extends Fragment{
         @Override
         public void onBindViewHolder(CrimeHolder holder, int position){
             Crime crime = mCrimes.get(position);
-            holder.mTitleTextView.setText(crime.getTitle());
+            // holder.mTitleTextView.setText(crime.getTitle());
+            holder.bindCrime(crime);
         }
 
         @Override

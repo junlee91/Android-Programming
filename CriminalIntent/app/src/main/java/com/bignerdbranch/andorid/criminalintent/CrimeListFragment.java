a6291210df83b2c0fe3fcf59a1ca5a103966c414
Jun Lee <jh.lee6@alticast.com>
2017-06-22 14:19:15 +0900

    saveInstanceState on rotation

--- CriminalIntent/app/src/main/java/com/bignerdbranch/andorid/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdbranch/andorid/criminalintent/CrimeListFragment.java
@@ -23,6 +23,8 @@ import java.util.List;
 
 public class CrimeListFragment extends Fragment{
 
+    private static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
+
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
     private boolean mSubtitleVisible;
@@ -41,6 +43,10 @@ public class CrimeListFragment extends Fragment{
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         // LayoutManager: Locating TextView items
 
+        if(savedInstanceState != null){
+            mSubtitleVisible = savedInstanceState.getBoolean(SAVED_SUBTITLE_VISIBLE);
+        }
+
         updateUI();
 
         return view;
@@ -50,6 +56,12 @@ public class CrimeListFragment extends Fragment{
     public void onResume(){
         super.onResume();
         updateUI();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SAVED_SUBTITLE_VISIBLE, mSubtitleVisible);
     }
 
     @Override
@@ -109,6 +121,7 @@ public class CrimeListFragment extends Fragment{
         } else {
             mAdapter.notifyDataSetChanged();
         }
+        updateSubtitle();
     }
 
     // ViewHolder

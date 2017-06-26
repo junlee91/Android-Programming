1f9556e0865d95719fd6e3fb6e7ed9fd85edf3cf
Jun Lee <jh.lee6@alticast.com>
2017-06-26 17:05:32 +0900

    smaple Imaview set with Bitmap

--- CriminalIntent/app/src/main/java/com/bignerdbranch/andorid/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdbranch/andorid/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -240,7 +241,7 @@ public class CrimeFragment extends Fragment {
         });
 
         mPhotoView = (ImageView)v.findViewById(R.id.crime_photo);
-
+        updatePhotoView();
 
         return v;
     }
@@ -297,6 +298,8 @@ public class CrimeFragment extends Fragment {
             } finally {
                 c.close();
             }
+        } else if( requestCode == REQUEST_PHOTO ){
+            updatePhotoView();
         }
     }
 
@@ -328,9 +331,14 @@ public class CrimeFragment extends Fragment {
         return report;
     }
 
-
-
-
-
+    private void updatePhotoView(){
+        if(mPhotoFile == null || !mPhotoFile.exists()){
+            mPhotoView.setImageDrawable(null);
+        } else {
+            Bitmap bitmap = PictureUtils.getScaledBitmap(
+                    mPhotoFile.getPath(), getActivity());
+            mPhotoView.setImageBitmap(bitmap);
+        }
+    }
 
 }

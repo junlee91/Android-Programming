d54ef517f1a4c45601b0ff68f11c0d6448b1ea57
Jun Lee <jh.lee6@alticast.com>
2017-06-27 17:09:58 +0900

    connect sound assets into SoundHolder/Adapter, BeatBoxFragment, SoundList

---
+++ BeatBox/app/src/main/java/com/bignerdbranch/andorid/beatbox/Sound.java
@@ -0,0 +1,26 @@
+package com.bignerdbranch.andorid.beatbox;
+
+/**
+ * Created by Jun on 2017-06-27.
+ */
+
+public class Sound {
+
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath){
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length-1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getmAssetPath(){
+        return mAssetPath;
+    }
+
+    public String getName(){
+        return mName;
+    }
+}

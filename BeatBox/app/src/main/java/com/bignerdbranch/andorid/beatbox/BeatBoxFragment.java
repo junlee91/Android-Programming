13cd1600d29726abc39722a9a21c78ba68b052ba
Jun Lee <jh.lee6@alticast.com>
2017-06-27 18:03:11 +0900

    Sound Play

--- BeatBox/app/src/main/java/com/bignerdbranch/andorid/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/bignerdbranch/andorid/beatbox/BeatBoxFragment.java
@@ -43,7 +43,8 @@ public class BeatBoxFragment extends Fragment {
     }
 
     // ViewHolder
-    private class SoundHolder extends RecyclerView.ViewHolder{
+    private class SoundHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener{
         private Button mButton;
         private Sound mSound;
 
@@ -51,11 +52,17 @@ public class BeatBoxFragment extends Fragment {
             super(inflater.inflate(R.layout.list_item_sound, container, false));
 
             mButton = (Button)itemView.findViewById(R.id.list_item_sound_button);
+            mButton.setOnClickListener(this);
         }
 
         public void bindSound(Sound sound){
             mSound = sound;
             mButton.setText(mSound.getName());
+        }
+
+        @Override
+        public void onClick(View v){
+            mBeatBox.play(mSound);
         }
     }
 

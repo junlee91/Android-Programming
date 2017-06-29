0f71c7029b29d2d8808de591d94c2ff3cdffe3c1
Jun Lee <jh.lee6@alticast.com>
2017-06-29 17:47:54 +0900

    AsyncTask doInbackground to load google.com

--- PhotoGallery/app/src/main/java/com/bignerdbranch/andorid/photogallery/FlickrFetchr.java
+++ PhotoGallery/app/src/main/java/com/bignerdbranch/andorid/photogallery/FlickrFetchr.java
@@ -12,28 +12,25 @@ import java.net.URL;
 
 public class FlickrFetchr {
 
-    public byte[] getUrlBytes(String urlSpec) throws IOException{
-
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 
-        try{
+        try {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             InputStream in = connection.getInputStream();
 
-            if(connection.getResponseCode() != HttpURLConnection.HTTP_OK){
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
                 throw new IOException(connection.getResponseMessage() +
-                ": with " +
-                urlSpec);
+                        ": with " +
+                        urlSpec);
             }
 
             int bytesRead = 0;
             byte[] buffer = new byte[1024];
-
-            while((bytesRead = in.read(buffer)) > 0){
+            while ((bytesRead = in.read(buffer)) > 0) {
                 out.write(buffer, 0, bytesRead);
             }
-
             out.close();
             return out.toByteArray();
         } finally {
--- PhotoGallery/app/src/main/java/com/bignerdbranch/andorid/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bignerdbranch/andorid/photogallery/PhotoGalleryFragment.java
@@ -1,19 +1,25 @@
 package com.bignerdbranch.andorid.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 /**
- * Created by NB201704112 on 2017-06-29.
+ * Created by Jun on 2017-06-29.
  */
 
 public class PhotoGalleryFragment extends Fragment {
+
+    private static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView mPhotoRecyclerView;
 
@@ -25,6 +31,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -38,5 +45,19 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected Void doInBackground(Void... params){
+            try{
+                String result = new FlickrFetchr()
+                        .getUrlString("http://www.google.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe){
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }

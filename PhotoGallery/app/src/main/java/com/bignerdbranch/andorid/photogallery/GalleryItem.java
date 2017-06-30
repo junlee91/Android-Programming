7e734294b4b3f7c8e42619bee15f7b119ab816d3
Jun Lee <jh.lee6@alticast.com>
2017-06-30 13:36:26 +0900

    parsing JSONObject

---
+++ PhotoGallery/app/src/main/java/com/bignerdbranch/andorid/photogallery/GalleryItem.java
@@ -0,0 +1,41 @@
+package com.bignerdbranch.andorid.photogallery;
+
+/**
+ * Created by Jun on 2017-06-30.
+ */
+
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    @Override
+    public String toString(){
+        return mCaption;
+    }
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String caption) {
+        mCaption = caption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+}

2d3fae9bf3efbd00a0458bf00fdd8640397e82b6
Jun <jlee757@uic.edu>
2017-07-05 11:26:04 +0900

    TODO::notification system

--- PhotoGallery/app/src/main/java/com/bignerdbranch/andorid/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/bignerdbranch/andorid/photogallery/PhotoGalleryFragment.java
@@ -58,7 +58,7 @@ public class PhotoGalleryFragment extends Fragment {
         // Intent i = PollService.newIntent(getActivity());
         // getActivity().startService(i);
 
-        PollService.setServiceAlarm(getActivity(), true);
+        // PollService.setServiceAlarm(getActivity(), true);
 
         Handler responseHandler = new Handler();
         mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
@@ -143,6 +143,13 @@ public class PhotoGalleryFragment extends Fragment {
                 searchView.setQuery(query, false);
             }
         });
+
+        MenuItem toggleItem = menu.findItem(R.id.menu_item_toggle_polling);
+        if(PollService.isServiceAlarmOn(getActivity())){
+            toggleItem.setTitle(R.string.start_polling);
+        } else {
+            toggleItem.setTitle(R.string.stop_polling);
+        }
     }
 
     @Override
@@ -151,6 +158,11 @@ public class PhotoGalleryFragment extends Fragment {
             case R.id.menu_item_clear:
                 QueryPreferences.setStoredQuery(getActivity(), null);
                 updateItems();
+                return true;
+            case R.id.menu_item_toggle_polling:
+                boolean shouldStartAlarm = !PollService.isServiceAlarmOn(getActivity());
+                PollService.setServiceAlarm(getActivity(), shouldStartAlarm);
+                getActivity().invalidateOptionsMenu();
                 return true;
             default:
                 return super.onOptionsItemSelected(menuItem);

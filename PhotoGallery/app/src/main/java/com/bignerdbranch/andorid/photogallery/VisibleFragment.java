5fbb6849d11d3294af91d96979c21b504c972f70
Jun <jlee757@uic.edu>
2017-07-06 18:08:19 +0900

    web and webview todo

--- PhotoGallery/app/src/main/java/com/bignerdbranch/andorid/photogallery/VisibleFragment.java
+++ PhotoGallery/app/src/main/java/com/bignerdbranch/andorid/photogallery/VisibleFragment.java
@@ -1,10 +1,12 @@
 package com.bignerdbranch.andorid.photogallery;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
@@ -31,10 +33,12 @@ public abstract class VisibleFragment extends Fragment {
     private BroadcastReceiver mOnShowNotification = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(getActivity(),
-                    "Got a broadcast: " + intent.getAction(),
-                    Toast.LENGTH_LONG)
-                    .show();
+//            Toast.makeText(getActivity(),
+//                    "Got a broadcast: " + intent.getAction(),
+//                    Toast.LENGTH_LONG)
+//                    .show();
+            Log.i(TAG, "canceling notification");
+            setResultCode(Activity.RESULT_CANCELED);
         }
     };
 }

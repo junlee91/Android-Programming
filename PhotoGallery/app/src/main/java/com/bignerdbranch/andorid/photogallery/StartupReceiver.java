5fb1b6820f6d7913ec21241170a7f05597b3c024
Jun <jlee757@uic.edu>
2017-07-06 16:15:51 +0900

    Broadcast Receiver

---
+++ PhotoGallery/app/src/main/java/com/bignerdbranch/andorid/photogallery/StartupReceiver.java
@@ -0,0 +1,19 @@
+package com.bignerdbranch.andorid.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Jun on 2017-07-06.
+ */
+
+public class StartupReceiver extends BroadcastReceiver {
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+    }
+}
